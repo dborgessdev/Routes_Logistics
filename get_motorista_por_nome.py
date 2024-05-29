3ee86a9d61b5448e929d194beb344d2b21d0dcff
rafaelcanjos <rafael_cost123@hotmail.com>
2024-05-29 11:16:56 -0300

    feat: desing patter get por nome

--- get_motorista_por_nome.py
+++ get_motorista_por_nome.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
 import requests
 import json
 
+class MotoristaDataManager:
+    _instance = None
+
+    def __new__(cls, link):
+        if cls._instance is None:
+            cls._instance = super(MotoristaDataManager, cls).__new__(cls)
+            cls._instance._link = link
+            cls._instance._data = None
+        return cls._instance
+
+    def get_data(self):
+        if self._data is None:
+            link_motorista = f'{self._link}/motoristas.json'
+            requisicao = requests.get(link_motorista)
+            self._data = requisicao.json()
+        return self._data
+
 def get_motorista_por_nome(nome, link):
-    link_motorista = f'{link}/motoristas.json'
-    requisicao = requests.get(link_motorista)
-    dados_motoristas = requisicao.json()
+    manager = MotoristaDataManager(link)
+    dados_motoristas = manager.get_data()
     
     for key, value in dados_motoristas.items():
         if value['nome'] == nome:
